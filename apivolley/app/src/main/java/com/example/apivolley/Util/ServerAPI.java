3f77dc3fddd5be271ae981084a3a4868deeee09f
Setiawan_Bagus <55245624+SetiawanBagus17@users.noreply.github.com>
2021-06-09 20:21:36 +0700

    Done

---
+++ apivolley/app/src/main/java/com/example/apivolley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.example.apivolley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.43.44/ci/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.43.44/ci/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.43.44/ci/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.43.44/ci/Mahasiswa/ApiUpdate";
+}
